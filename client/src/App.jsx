7cc69585629e6b1280022f222f55169b12e9465e
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2023-10-25 13:22:57 +0530

    c

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,22 @@
-
 import "./App.css";
 import Home from "./pages/user/home/Home";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import homeRoute from '../src/pages/user/home/HomeRoutes'
+import homeRoute from "../src/pages/user/home/HomeRoutes";
 
-
-
-const App=()=> {
-  const [routes, setRoutes] = useState([{ path: '/*', element: <Home height={200} width={100} /> }]);
-  useEffect(()=>{
-    setRoutes(
-      homeRoute
-    )
-
-  },[])
+const App = () => {
+  const [routes, setRoutes] = useState([
+    { path: "/*", element: <Home height={200} width={100} /> },
+  ]);
+  useEffect(() => {
+    setRoutes(homeRoute);
+  }, []);
   return (
     <>
-     <RouterProvider router={createBrowserRouter(routes)} />
+      <RouterProvider router={createBrowserRouter(routes)} />
       {/* <Home /> */}
-
     </>
   );
-}
+};
 
 export default App;

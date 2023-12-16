f8675153e7e6b20c19ff4fef0d74c45ec339d187
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2023-12-16 23:46:13 +0530

    app changes

--- server/app.js
+++ server/app.js
@@ -47,7 +47,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 mongoose
-  .connect(process.env.MONGO_DB ?? 'mongodb://127.0.0.1:27017/letzFlix')
+  .connect(process.env.MONGO_DB)
   .then(async () => {
     console.log('Connected to mongodb');
     console.log('Connected to mongodb');
